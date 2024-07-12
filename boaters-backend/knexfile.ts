003261a0023b87dea9d77e8ea29b155611de76fa
Holden-VanKeyes <102389369+Holden-VanKeyes@users.noreply.github.com>
2024-07-11 19:11:10 -0500

    more setup
    knex - db setup - docker - first migration

--- boaters-backend/knexfile.ts
+++ boaters-backend/knexfile.ts
@@ -1,55 +1,20 @@
+require('dotenv').config({ path: '.env.development' })
+
 import type { Knex } from 'knex'
 
 // Update with your config settings.
+console.log('URL', process.env.POSTGRES_URL)
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: process.env.DB_URL,
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'dev',
+  },
   migrations: {
     extension: 'ts',
   },
 }
 
-// const config: { [key: string]: Knex.Config } = {
-//   development: {
-//     client: "sqlite3",
-//     connection: {
-//       filename: "./dev.sqlite3"
-//     }
-//   },
-
-//   staging: {
-//     client: "postgresql",
-//     connection: {
-//       database: "my_db",
-//       user: "username",
-//       password: "password"
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: "knex_migrations"
-//     }
-//   },
-
-//   production: {
-//     client: "postgresql",
-//     connection: {
-//       database: "my_db",
-//       user: "username",
-//       password: "password"
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: "knex_migrations"
-//     }
-//   }
-
-// };
-
-// module.exports = config;
+export default config
---
+++ boaters-backend/src/utils/postgres.ts
@@ -0,0 +1,8 @@
+import { Pool } from 'pg'
+
+const pool = new Pool({
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  database: 'mydb',
+})
