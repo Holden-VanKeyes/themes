fbf2d88329551bb48fef756a91925e86fa81f158
Holden-VanKeyes <102389369+Holden-VanKeyes@users.noreply.github.com>
2024-11-29 22:49:05 -0600

    fix TS error

--- knexfile.ts
+++ knexfile.ts
@@ -11,9 +11,26 @@ const config: Knex.Config = {
     port: 5432,
     database: 'postgres',
   },
+  //@ts-ignore
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
   migrations: {
     extension: 'ts',
   },
 }
+
+// const config: { [key: string]: Knex.Config } = {
+//   development: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL || {
+//       host: '0.0.0.0',
+//       port: 5432,
+//       database: 'postgres',
+//     },
+//     ssl: { rejectUnauthorized: false },
+//     migrations: {
+//       extension: 'ts',
+//     }
+//   }
+// };
 
 export default config
